5d596adbd26107842607fff90e7cb552df4ad6e0
PraveenK94 <praveen_kumaresan@gmail.com>
2019-09-18 20:10:04 +0530

    Fic

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,15 +4,19 @@ import Toolbar from "@material-ui/core/Toolbar";
 import MenuIcon from "@material-ui/icons/Menu";
 
 import IconButton from "@material-ui/core/IconButton";
+import { Typography } from "@material-ui/core";
 export default class Header extends React.Component {
   render() {
     return (
-      <AppBar position="absolute">
+      <AppBar position="fixed">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="open drawer">
-            <MenuIcon />
-          </IconButton>
-          <IconButton color="inherit"></IconButton>
+          <IconButton></IconButton>
+          <IconButton></IconButton>
+          <IconButton></IconButton>
+
+          <Typography color="inherit" variant="h4" align="right">
+            Water Meter Monitoring
+          </Typography>
         </Toolbar>
       </AppBar>
     );
